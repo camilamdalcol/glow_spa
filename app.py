9cbe097ccc1203e7eff7f0817431d0c15bae95ba
Camila Dal Col <152093967+camilamdalcol@users.noreply.github.com>
2024-06-10 22:05:27 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
         db.create_all()
 
     # Run the Flask application
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='127.0.0.1', port=8080)
